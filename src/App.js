a5cc6e7d539b100a80c1c469adcd7ddef7e10620
GeoFelix <felixodhiambo538@gmail.com>
2023-09-03 09:47:38 +0300

    first react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import flower from './images/flower.jpg';
+import { Button, Card } from 'react-bootstrap';
+
+// import './App.css';
+
+const divStyle={
+  display:'flex',
+  height: '100vh',
+  justifyContent:'center',
+  alignItems: 'center'
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"
+    style={divStyle}
+    >
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={flower} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
     </div>
   );
 }
