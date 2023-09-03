b0890eee4dae62b6670c5d75b66db181020e45f6
GeoFelix <felixodhiambo538@gmail.com>
2023-09-03 18:07:42 +0300

    made some changes

--- src/Rbst02.js
+++ src/Rbst02.js
@@ -13,7 +13,6 @@ const featureIcon={
     width: '4rem',
     height: '4rem',
     borderRadius: '0.75rem',
-    
   }
 const divStyle={
     alignItems:'start !important'
@@ -148,7 +147,6 @@ const Rbst02 =()=>{
                     <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
                     </ul>
                 </div>
-
                 <div class="col-6 col-md-2 mb-3">
                     <h5>Section</h5>
                     <ul class="nav flex-column">
@@ -171,10 +169,8 @@ const Rbst02 =()=>{
                     </form>
                 </div>
                 </div>
-
                 <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
                 <p>© 2023 Company, Inc. All rights reserved.</p>
-                
                 </div>
             </footer>
             </Container>
