bd2fe12dedebc7a80beec204b923637d5b64ff50
GeoFelix <felixodhiambo538@gmail.com>
2023-09-03 11:57:12 +0300

    Navebar working as expected

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import flower from './images/flower.jpg';
-import { Button, Card } from 'react-bootstrap';
+import './App.css';
+import Rbst01 from './Rbst01';
+import Rbst02 from './Rbst02';
 
-// import './App.css';
-
-const divStyle={
-  display:'flex',
-  height: '100vh',
-  justifyContent:'center',
-  alignItems: 'center'
-
-}
 
 function App() {
   return (
-    <div className="App"
-    style={divStyle}
-    >
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={flower} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+    <div className="App">
+        <Rbst02/>
     </div>
   );
 }
