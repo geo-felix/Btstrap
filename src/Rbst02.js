c72537b630b62141c5a60bfb7e515c6fa31958cd
GeoFelix <felixodhiambo538@gmail.com>
2023-09-03 13:28:35 +0300

    feature is ok only that aligning items is still work to be done

--- src/Rbst02.js
+++ src/Rbst02.js
@@ -7,6 +7,18 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from './images/box-seam.svg'
 import move from './images/moving-van.jpg'
+import briefcase from './images/briefcase.svg'
+import bus from './images/bus-front.svg'
+import heart from './images/chat-square-heart.svg'
+const featureIcon={
+    width: '4rem',
+    height: '4rem',
+    borderRadius: '0.75rem',
+    
+  }
+const divStyle={
+    alignItems:'start !important'
+}
 
 const Rbst02 =()=>{
     return(
@@ -61,6 +73,53 @@ const Rbst02 =()=>{
                 <div class="overflow-hidden" style={{maxHeight: '30vh'}}>
                     <div class="container px-5">
                         <img src={move} class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" style={{width:"700", height:"500"}} loading="lazy"/>
+                    </div>
+                </div>
+            </div>
+            <div  class="container px-4 py-5" id="featured-3">
+                <h2  className="pb-2 border-bottom ">Why Move With Us?</h2>
+                <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+                    <div style={divStyle} class="feature col">
+                        <div style={featureIcon} class="d-inline-flex align-items-center justify-content-center
+                         text-bg-primary bg-gradient fs-2 mb-3">
+                            <img src={briefcase} style={{height:'30px'}} alt="logo image"/>
+                        </div>
+                        <h3 class="fs-2 text-body-emphasis">Professional</h3>
+                        <p>Our team of professional movers are trained to prioritize efficiency, 
+                            organization, and attention to detail. We understand that your possessions 
+                            are more than just objects - they are a reflection of your life, memories, 
+                            and experiences. That's why we take extra care to ensure that everything is packed 
+                            and transported with the utmost care and attention.
+                        </p>
+                        <a href="#" class="icon-link">
+                        Get a quote
+                            <svg class="bi"></svg>
+                        </a>
+                    </div>
+                    <div class="feature col">
+                        <div style={featureIcon} class=" d-inline-flex align-items-center justify-content-center
+                         text-bg-primary bg-gradient fs-2 mb-3">
+                            <img src={bus} style={{height:'30px'}} alt="bus image"/>
+                        </div>
+                        <h3 class="fs-2 text-body-emphasis">Countrywide</h3>
+                        <p>We offer a range of services to suit your individual needs, whether you're moving locally or across the country. Our team can handle everything from packing and loading to unloading and unpacking, so you can focus on settling into your new home. And with our transparent pricing and no hidden fees, you can trust that you're getting a fair and competitive rate for our services.
+                        </p>
+                        <a href="#" class="icon-link">
+                        Get a quote
+                            <svg class="bi"></svg>
+                        </a>
+                    </div>
+                    <div class="feature col">
+                        <div style={featureIcon} class=" d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                            <img src={heart} style={{height:'30px'}} alt="logo image"/>
+                        </div>
+                        <h3 class="fs-2 text-body-emphasis">Personal Touch</h3>
+                            <p>At our core, we believe that moving should be an exciting and positive experience, not a stressful one. By providing exceptional moving services in the US, we hope to revolutionize the way people think about moving and provide a better, more personalized experience for our customers. Contact us today to learn more about how we can help you with your next move.
+                            </p>
+                        <a href="#" class="icon-link">
+                        Get a quote
+                            <svg class="bi"></svg>
+                        </a>
                     </div>
                 </div>
             </div>
